795cf90a6cee470a098ff2a3fe898112f40974d4
Antón Molleda <amolleda@gmail.com>
2019-09-25 04:35:02 -0700

    New: Tweaks

--- serve.js
+++ serve.js
@@ -2,9 +2,9 @@ var static = require('node-static');
 var file = new static.Server('./');
 
 require('http').createServer(function (request, response) {
+    console.log('Serving on http://localhost:4200/');
+
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
 }).listen(4200);
-
-console.log('Serving on http://localhost:4200/');
